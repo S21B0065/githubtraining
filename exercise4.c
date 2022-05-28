0ff8708fec2c5529bcdec79fe8d95e8a8de4e02c
S21B0065 <96066234+S21B0065@users.noreply.github.com>
2022-05-28 11:57:07 +0800

    Add files via upload

---
+++ exercise4.c
@@ -0,0 +1,36 @@
+//documentation section 
+/* Exercise 4 - Insertion Operations */
+
+//pre-processor section 
+#include<stdio.h>
+
+//global variable section 
+
+
+//main function section 
+int main(){
+    //write here your program
+char LA[] = {'a', 'e', 'i', 'o'};
+    int N = 4;
+    int i = 0;
+    int index = 4;
+    char value = 'u';
+
+    for(i = 0; i < N; i++){
+    printf("LA[%d] = %c \n", i, LA[i]);
+
+    }
+
+    N++;
+
+    LA[N-1] = value;
+
+    printf("Output : \n");
+
+    for(i = 0; i < N; i++){
+        printf("LA[%d] = %c\n", i, LA[i]);
+    }
+
+    return 0;
+}
+    
